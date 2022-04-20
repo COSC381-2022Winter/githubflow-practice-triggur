3b211cb2d3e8d39f56cf600597e4b0ec84dca112
triggur <sbork1@emich.edu>
2022-04-19 22:01:17 -0400

    fix: fixed commenting typo error
    Fixed the commenting typo error where the comment was not properly
    defined and was missing the pound symbol.
    
    issue #2

--- flaskr/movies.py
+++ flaskr/movies.py
@@ -4,6 +4,6 @@ from flaskr.db import get_movies
 bp = Blueprint('movies', __name__, url_prefix='/movies')    #Sets bp to flask's Blueprint module
 
 @bp.route('/', methods=['GET'])
-def movie_list():   Defines the movie list, gets the movies, and returns them under a template
+def movie_list():   #Defines the movie list, gets the movies, and returns them under a template
     movies = get_movies()
     return render_template('movies.html', movies=movies)
