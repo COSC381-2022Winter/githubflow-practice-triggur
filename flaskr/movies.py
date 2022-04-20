a59ad5e4c97a9d7644e62077642b0ef02d28e1a1
triggur <sbork1@emich.edu>
2022-04-19 21:31:17 -0400

    fix: add comments to movies.py code blocks
    Added Comments into the movies.py code blocks to sate the tasks of the
    issue submitted to the repository.
    
    issue #1

--- flaskr/movies.py
+++ flaskr/movies.py
@@ -1,9 +1,9 @@
-from flask import Blueprint, request, render_template
+from flask import Blueprint, request, render_template   #Import data from flask
 from flaskr.db import get_movies
 
-bp = Blueprint('movies', __name__, url_prefix='/movies')
+bp = Blueprint('movies', __name__, url_prefix='/movies')    #Set bp to the Blueprint module
 
 @bp.route('/', methods=['GET'])
-def movie_list():
+def movie_list():   #Define a Movie list and return a rendered template of the movies
     movies = get_movies()
     return render_template('movies.html', movies=movies)
